e1c36d2283c2297a26bf6b546298628ba9879101
sanarzd <sana.rezazadeh.email@gmail.com>
2026-05-24 22:47:30 +0330

    remove file from repo

--- ex6.py
+++
@@ -1,8 +0,0 @@
-def mysum(numbers, start = 0):
-    output = start
-    for number in numbers:
-        output += number
-    return output
-
-print(mysum([10, 20, 30, 40]))
-print(mysum([10, 20, 30, 40], 50))    
